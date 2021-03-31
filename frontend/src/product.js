8e3bc1e1e8765ac78bd03d1871baf8d10895684d
tiabeanie <sydney.jane.hunter@gmail.com>
2021-03-31 15:21:35 -0600

    added product js

---
+++ frontend/src/product.js
@@ -0,0 +1,23 @@
+class Category {
+    constructor(dataObject) {
+      this.id = dataObject.id;
+      this.name = dataObject.name;
+      this.products = dataObject.items;
+    }
+  
+    renderIndexCategory() {
+      return `
+          <li class="py-2">
+               <div class="flex space-x-3">
+                 <div class="flex-1 space-y-1">
+                   <div class="flex products-center justify-between">
+                     <h3 class="text-sm font-medium hover:text-gray-400 products-link">${this.name}</h3>
+                   </div>
+                   <p class="text-sm text-gray-500">${this.products.length} products</p>
+                  </svg>
+                 </div>
+               </div>
+             </li>
+          `;
+    }
+  }
